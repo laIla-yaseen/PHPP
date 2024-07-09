453baebe75e6e487c71c6063d1a015b652dda6b6
unknown <lailaeasen2244@gmail.com>
2024-07-09 14:41:43 +0300

    7-9

--- indexe.php
+++ indexe.php
@@ -1,3 +1,4 @@
+
 <?php
 
 //task 1
@@ -143,17 +144,17 @@ $students = [
 // task 4 
 
 $colors = ["red", "yallow", "green"];
-//1
+//1  لإضافة عنصر إلى مصفوفة مؤشرة
 // array_push($colors , "brown");
 
-//2
+//2 إزالة العنصر الأخير من مصفوفة
 // array_pop($colors);
 
 // echo '<pre>';
 // print_r($colors);
 // echo '</pre>';
 
-//3
+//3 حصول على جميع المفاتيح في مصفوفة
 $age = [
     "A" => 10,
     "B" => 20,
@@ -166,17 +167,113 @@ $age = [
 // print_r($key); 
 // echo '</pre>';
 
-//4
+//4  لحصول على جميع القيم في مصفوفة
 // array_values($age);
 
 // echo '<pre>';
 // print_r($age);
 // echo '</pre>';
 
-//5
+//5  للحصول على عدد العناصر في مصفوفة
 // $gg =count($colors);
 
 // echo '<pre>';
 // print_r($gg);
 // echo '</pre>';
+
+
+//6  لدمج مصفوفتين
+
+// $kk =array_merge($colors,$age );
+
+// echo '<pre>';
+// print_r($kk);
+// echo '</pre>';
+
+
+//7 للبحث عن قيمة محددة في مصفوفة
+
+
+// $ll= array_search("green", $colors );
+
+// echo '<pre>';
+// print_r($ll);
+// echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// task 5
+
+$cities = ["Amman", "Aqaba", "Karak", "Zarqa", "Irbid", "Ajloun"];
+
+// طباعة عناصر المصفوفة بترتيب عكسي
+// $aa = array_reverse($cities);
+
+//  echo '<pre>';
+//  print_r($aa);
+//  echo '</pre>';
+
+
+//للبحث عن قيمة محددة
+
+// $bb = "Karak";
+
+// $cc = array_search($bb,$cities);
+// // echo '<pre>';
+// print_r($cc);
+// // echo '</pre>';
+
+
+// 
+
+// $dd = "Z44arqa";
+// $kay = array_search($dd,$cities );
+
+// if ($kay) {
+//     unset($cities[$kay]);
+//     echo "The city $dd was removed successfully.";
+// } else{
+//     echo "The city $dd does not exist in the array.";
+// };
+
+// echo '<pre>';
+// print_r($cities);
+// echo '</pre>';
+
+
+
+// استخراج جزء من المصفوفة للحصول على مصفوفة فرعية 
+
+// $ee = array_slice($cities, 0,3);
+
+// echo '<pre>';
+// print_r($ee);
+// echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
+
 
